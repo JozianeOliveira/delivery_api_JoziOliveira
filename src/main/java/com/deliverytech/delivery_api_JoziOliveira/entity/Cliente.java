1263fe14ac24898fd42eb8d170ae18dd2d036f9f
Joziane Oliveira <jozioliveira@edu.uniube.br>
2026-05-07 21:36:28 -0300

    Create Cliente.java

---
+++ src/main/java/com/deliverytech/delivery_api_JoziOliveira/entity/Cliente.java
@@ -0,0 +1,23 @@
+package com.deliverytech.delivery_api_JoziOliveira.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.List;
+
+@Entity
+@Data
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+    private String endereco;
+    private boolean ativo;
+
+    @OneToMany(mappedBy = "cliente")
+    private List<Pedido> pedidos;
+
+}
