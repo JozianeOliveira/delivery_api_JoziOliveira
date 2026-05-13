2a65b44c5445ab57782f88556d33003f6672ec8d
Joziane Oliveira <jozioliveira@edu.uniube.br>
2026-05-12 21:36:34 -0300

    Update Produto.java

--- src/main/java/com/deliverytech/delivery_api_JoziOliveira/entity/Produto.java
+++ src/main/java/com/deliverytech/delivery_api_JoziOliveira/entity/Produto.java
@@ -1,22 +1,34 @@
 package com.deliverytech.delivery_api_JoziOliveira.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
- 
+import java.util.List;
+
 @Entity
 @Data
 public class Produto {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     private String nome;
     private String descricao;
     private double preco;
     private String categoria;
     private boolean disponivel;
- 
+    private Integer validade;
+
     @ManyToOne
     @JoinColumn(name = "restaurante_id")
     private Restaurante restaurante;
- 
+
+    @OneToMany(mappedBy = "produto")
+    private List<ItemPedido> itensPedido;
 }
