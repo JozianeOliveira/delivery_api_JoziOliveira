db9039ee3216ef65cc0d24a8d9f3761ba45a472f
Joziane Oliveira <jozioliveira@edu.uniube.br>
2026-05-18 22:24:01 -0300

    Create ModelMapperConfig.java

---
+++ src/main/java/com/deliverytech/delivery_api_JoziOliveira/config/ModelMapperConfig.java
@@ -0,0 +1,22 @@
+package com.deliverytech.delivery_api_JoziOliveira.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper mapper = new ModelMapper();
+    
+
+        // cofigurações adicionais do ModelMapper podem ser feitas aqui, se necessário
+        mapper.getConfiguration()
+        .setFieldMatchingEnabled(true)
+        .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE);
+    
+        return mapper;
+    }
+}
